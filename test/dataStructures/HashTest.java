2a9c5c82fc8fbace631692c461550e2e1cf90b05
EstebanGZam <estebangaviria82@gmail.com>
2023-04-30 09:19:32 -0500

    Integrated changes with master

--- test/dataStructures/HashTest.java
+++ test/dataStructures/HashTest.java
@@ -13,134 +13,134 @@ import static org.junit.Assert.fail;
 
 public class HashTest {
 
-    private Hash<String, Passenger> passengerHash;
+	private Hash<String, Passenger> passengerHash;
 
-    // Objects used
-    private final int NUMBER_OF_PASSENGERS = 5;
-    private Passenger p1 = new Passenger("Miguel", "Torres", "12345", PassengerClass.ECONOMY, "A8", 18, 50, false, false);
-    private Passenger p2 = new Passenger("Daniel", "Gonzales", "23456", PassengerClass.FIRST_CLASS, "B1", 22, 1200, false, false);
-    private Passenger p3 = new Passenger("Maria", "Gomez", "34567", PassengerClass.FIRST_CLASS, "C2", 40, 150, true, false);
-    private Passenger p4 = new Passenger("Juan", "Perez", "45678", PassengerClass.ECONOMY, "B5", 33, 0, false, false);
-    private Passenger p5 = new Passenger("Javier", "Roman", "56789", PassengerClass.ECONOMY, "C6", 75, 3000, true, false);
+	// Objects used
+	private final int NUMBER_OF_PASSENGERS = 5;
+	private final Passenger p1 = new Passenger("Miguel", "Torres", "12345", PassengerClass.ECONOMY, "A8", 18, 50, false, false);
+	private final Passenger p2 = new Passenger("Daniel", "Gonzales", "23456", PassengerClass.FIRST_CLASS, "B1", 22, 1200, false, false);
+	private final Passenger p3 = new Passenger("Maria", "Gomez", "34567", PassengerClass.FIRST_CLASS, "C2", 40, 150, true, false);
+	private final Passenger p4 = new Passenger("Juan", "Perez", "45678", PassengerClass.ECONOMY, "B5", 33, 0, false, false);
+	private final Passenger p5 = new Passenger("Javier", "Roman", "56789", PassengerClass.ECONOMY, "C6", 75, 3000, true, false);
 
-    // Setups
-    public void setup1() {
-        passengerHash = new Hash<>(NUMBER_OF_PASSENGERS);
-    }
+	// Setups
+	public void setup1() {
+		passengerHash = new Hash<>(NUMBER_OF_PASSENGERS);
+	}
 
-    public void setup2() {
-        passengerHash = new Hash<>(NUMBER_OF_PASSENGERS);
-        try {
-            passengerHash.insert(p1.getIdentification(), p1);
-            passengerHash.insert(p2.getIdentification(), p2);
-        } catch (HashException he) {
-            fail(he.getMessage());
-        }
-    }
+	public void setup2() {
+		passengerHash = new Hash<>(NUMBER_OF_PASSENGERS);
+		try {
+			passengerHash.insert(p1.getIdentification(), p1);
+			passengerHash.insert(p2.getIdentification(), p2);
+		} catch (HashException he) {
+			fail(he.getMessage());
+		}
+	}
 
-    public void setup3() {
-        passengerHash = new Hash<>(NUMBER_OF_PASSENGERS);
-        try {
-            passengerHash.insert(p1.getIdentification(), p1);
-            passengerHash.insert(p2.getIdentification(), p2);
-            passengerHash.insert(p3.getIdentification(), p3);
-            passengerHash.insert(p4.getIdentification(), p4);
-            passengerHash.insert(p5.getIdentification(), p5);
-        } catch (HashException he) {
-            fail(he.getMessage());
-        }
-    }
+	public void setup3() {
+		passengerHash = new Hash<>(NUMBER_OF_PASSENGERS);
+		try {
+			passengerHash.insert(p1.getIdentification(), p1);
+			passengerHash.insert(p2.getIdentification(), p2);
+			passengerHash.insert(p3.getIdentification(), p3);
+			passengerHash.insert(p4.getIdentification(), p4);
+			passengerHash.insert(p5.getIdentification(), p5);
+		} catch (HashException he) {
+			fail(he.getMessage());
+		}
+	}
 
-    // Insert(): test 1
-    @Test
-    public void testInsertTwoObjects() {
-        setup1();
-        try {
-            passengerHash.insert(p1.getIdentification(), p1);
-            passengerHash.insert(p2.getIdentification(), p2);
-            assert 2 == passengerHash.size();
-        } catch (Exception e) {
-            fail(e.getMessage());
-        }
-    }
+	// Insert(): test 1
+	@Test
+	public void testInsertTwoObjects() {
+		setup1();
+		try {
+			passengerHash.insert(p1.getIdentification(), p1);
+			passengerHash.insert(p2.getIdentification(), p2);
+			assert 2 == passengerHash.size();
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
 
-    // Insert(): test 2
-    @Test
-    public void testInsertHashOverflow() {
-        setup3();
-        Passenger p6 = new Passenger("Esteban", "GZam", "54321", PassengerClass.FIRST_CLASS, "A1", 18, 2000, false, false);
-        assertThrows(HashException.class, () -> passengerHash.insert(p6.getIdentification(), p6));
-    }
+	// Insert(): test 2
+	@Test
+	public void testInsertHashOverflow() {
+		setup3();
+		Passenger p6 = new Passenger("Esteban", "GZam", "54321", PassengerClass.FIRST_CLASS, "A1", 18, 2000, false, false);
+		assertThrows(HashException.class, () -> passengerHash.insert(p6.getIdentification(), p6));
+	}
 
-    // Insert(): test 3
-    @Test
-    public void testInsertElementWithTheSameKey() {
-        setup2();
+	// Insert(): test 3
+	@Test
+	public void testInsertElementWithTheSameKey() {
+		setup2();
 
-        Passenger p6 = new Passenger("Juan", "jdColonia", "12345", PassengerClass.FIRST_CLASS, "A3", 18, 3500, false, false);
-        assertThrows(HashException.class, () -> passengerHash.insert(p6.getIdentification(), p6));
-    }
+		Passenger p6 = new Passenger("Juan", "jdColonia", "12345", PassengerClass.FIRST_CLASS, "A3", 18, 3500, false, false);
+		assertThrows(HashException.class, () -> passengerHash.insert(p6.getIdentification(), p6));
+	}
 
-    // Search(): test 1
-    @Test
-    public void testSearchElement() {
-        setup2();
-        try {
-            Passenger p6 = new Passenger("Santiago", "santiesleo", "98765", PassengerClass.FIRST_CLASS, "B2", 19, 1500, false, false);
-            passengerHash.insert(p6.getIdentification(), p6);
-            assertEquals(p6, passengerHash.search("98765"));
-        } catch (Exception e) {
-            fail(e.getMessage());
-        }
-    }
+	// Search(): test 1
+	@Test
+	public void testSearchElement() {
+		setup2();
+		try {
+			Passenger p6 = new Passenger("Santiago", "santiesleo", "98765", PassengerClass.FIRST_CLASS, "B2", 19, 1500, false, false);
+			passengerHash.insert(p6.getIdentification(), p6);
+			assertEquals(p6, passengerHash.search("98765"));
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
 
-    // Search(): test 2
-    @Test
-    public void testSearchElementInEmptyHash() {
-        setup1();
-        assertNull(passengerHash.search(p1.getIdentification()));
-    }
+	// Search(): test 2
+	@Test
+	public void testSearchElementInEmptyHash() {
+		setup1();
+		assertNull(passengerHash.search(p1.getIdentification()));
+	}
 
-    // Search(): test 3
-    @Test
-    public void testSearchWithNoEnteredKey() {
-        setup3();
-        assertNull(passengerHash.search("54321"));
-    }
+	// Search(): test 3
+	@Test
+	public void testSearchWithNoEnteredKey() {
+		setup3();
+		assertNull(passengerHash.search("54321"));
+	}
 
-    // Delete(): test1
-    @Test
-    public void testDeleteTheUniqueObjectInserted() {
-        setup1();
-        try {
-            passengerHash.insert(p1.getIdentification(), p1);
-            passengerHash.delete(p1.getIdentification());
-            assert 0 == passengerHash.size();
-        } catch (HashException he) {
-            fail(he.getMessage());
-        }
-    }
+	// Delete(): test 1
+	@Test
+	public void testDeleteTheUniqueObjectInserted() {
+		setup1();
+		try {
+			passengerHash.insert(p1.getIdentification(), p1);
+			passengerHash.delete(p1.getIdentification());
+			assert 0 == passengerHash.size();
+		} catch (HashException he) {
+			fail(he.getMessage());
+		}
+	}
 
-    // Delete(): test2
-    @Test
-    public void testDeleteWithNotEnteredObjects() {
-        setup3();
-        passengerHash.delete("46789");
-        passengerHash.delete("98765");
-        assert 5 == passengerHash.size();
-    }
+	// Delete(): test 2
+	@Test
+	public void testDeleteWithNotEnteredObjects() {
+		setup3();
+		passengerHash.delete("46789");
+		passengerHash.delete("98765");
+		assert 5 == passengerHash.size();
+	}
 
-    // Delete(): test3
-    @Test
-    public void testDeleteMultipleElements() {
-        setup3();
-        passengerHash.delete("12345");
-        passengerHash.delete("23456");
-        passengerHash.delete("45678");
-        assertNull(passengerHash.search("12345"));
-        assertNull(passengerHash.search("23456"));
-        assertNull(passengerHash.search("45678"));
-        assert 2 == passengerHash.size();
-    }
+	// Delete(): test 3
+	@Test
+	public void testDeleteMultipleElements() {
+		setup3();
+		passengerHash.delete("12345");
+		passengerHash.delete("23456");
+		passengerHash.delete("45678");
+		assertNull(passengerHash.search("12345"));
+		assertNull(passengerHash.search("23456"));
+		assertNull(passengerHash.search("45678"));
+		assert 2 == passengerHash.size();
+	}
 
 }
--- test/dataStructures/PriorityQueueTest.java
+++ test/dataStructures/PriorityQueueTest.java
@@ -11,145 +11,145 @@ import static org.junit.Assert.assertNull;
 
 public class PriorityQueueTest {
 
-    private PriorityQueue<Integer, Passenger> priorityQueue;
+	private PriorityQueue<Integer, Passenger> priorityQueue;
 
-    // Objects used
-    private Passenger p1 = new Passenger("Juan", "jdColonia", "12345", PassengerClass.ECONOMY, "A8", 18, 50, false, false);
-    private Passenger p2 = new Passenger("Santiago", "santiesleo", "23456", PassengerClass.FIRST_CLASS, "B1", 22, 1200, false, false);
-    private Passenger p3 = new Passenger("Esteban", "EstebanGZam", "34567", PassengerClass.FIRST_CLASS, "C2", 40, 150, true, false);
-    private Passenger p4 = new Passenger("Javier", "Roman", "56789", PassengerClass.ECONOMY, "C6", 75, 3000, true, false);
-    private Passenger p5 = new Passenger("Juan", "Perez", "45678", PassengerClass.ECONOMY, "B5", 33, 0, false, false);
+	// Objects used
+	private final Passenger p1 = new Passenger("Juan", "jdColonia", "12345", PassengerClass.ECONOMY, "A8", 18, 50, false, false);
+	private final Passenger p2 = new Passenger("Santiago", "santiesleo", "23456", PassengerClass.FIRST_CLASS, "B1", 22, 1200, false, false);
+	private final Passenger p3 = new Passenger("Esteban", "EstebanGZam", "34567", PassengerClass.FIRST_CLASS, "C2", 40, 150, true, false);
+	private final Passenger p4 = new Passenger("Javier", "Roman", "56789", PassengerClass.ECONOMY, "C6", 75, 3000, true, false);
+	private final Passenger p5 = new Passenger("Juan", "Perez", "45678", PassengerClass.ECONOMY, "B5", 33, 0, false, false);
 
-    // Setups
-    public void setup1() {
-        priorityQueue = new PriorityQueue<>();
-    }
+	// Setups
+	public void setup1() {
+		priorityQueue = new PriorityQueue<>();
+	}
 
-    public void setup2() {
-        priorityQueue = new PriorityQueue<>();
-        priorityQueue.insert(40, p2);
-        priorityQueue.insert(30, p3);
-        priorityQueue.insert(50, p1);
-    }
+	public void setup2() {
+		priorityQueue = new PriorityQueue<>();
+		priorityQueue.insert(40, p2);
+		priorityQueue.insert(30, p3);
+		priorityQueue.insert(50, p1);
+	}
 
-    public void setup3() {
-        priorityQueue = new PriorityQueue<>();
-        priorityQueue.insert(42, p1);
-        priorityQueue.insert(23, p2);
-        priorityQueue.insert(56, p3);
-        priorityQueue.insert(56, p4);
-        priorityQueue.insert(40, p5);
-    }
+	public void setup3() {
+		priorityQueue = new PriorityQueue<>();
+		priorityQueue.insert(42, p1);
+		priorityQueue.insert(23, p2);
+		priorityQueue.insert(56, p3);
+		priorityQueue.insert(56, p4);
+		priorityQueue.insert(40, p5);
+	}
 
-    // Insert(): test1
-    @Test
-    public void testInsertIntoEmptyQueue() {
-        setup1();
-        priorityQueue.insert(42, p1);
-        assert 1 == priorityQueue.size();
-        assertEquals(p1, priorityQueue.extractMax());
-    }
+	// Insert(): test 1
+	@Test
+	public void testInsertIntoEmptyQueue() {
+		setup1();
+		priorityQueue.insert(42, p1);
+		assert 1 == priorityQueue.size();
+		assertEquals(p1, priorityQueue.extractMax());
+	}
 
-    // Insert(): test2
-    @Test
-    public void testInsertMultipleElements() {
-        setup1();
-        priorityQueue.insert(42, p1);
-        priorityQueue.insert(23, p2);
-        priorityQueue.insert(56, p3);
-        assert 3 == priorityQueue.size();
-        assertEquals(p3, priorityQueue.extractMax());
-    }
+	// Insert(): test 2
+	@Test
+	public void testInsertMultipleElements() {
+		setup1();
+		priorityQueue.insert(42, p1);
+		priorityQueue.insert(23, p2);
+		priorityQueue.insert(56, p3);
+		assert 3 == priorityQueue.size();
+		assertEquals(p3, priorityQueue.extractMax());
+	}
 
-    // Insert(): test3
-    @Test
-    public void testInsertElementsWithSamePriority() {
-        setup1();
-        priorityQueue.insert(42, p1);
-        priorityQueue.insert(56, p2);
-        priorityQueue.insert(56, p3);
-        priorityQueue.insert(23, p4);
-        assert 4 == priorityQueue.size();
-        assertEquals(p2, priorityQueue.extractMax());
-    }
+	// Insert(): test 3
+	@Test
+	public void testInsertElementsWithSamePriority() {
+		setup1();
+		priorityQueue.insert(42, p1);
+		priorityQueue.insert(56, p2);
+		priorityQueue.insert(56, p3);
+		priorityQueue.insert(23, p4);
+		assert 4 == priorityQueue.size();
+		assertEquals(p2, priorityQueue.extractMax());
+	}
 
-    // Maximum(): test1
-    @Test
-    public void testCorrectMaximum() {
-        setup2();
-        assert 3 == priorityQueue.size();
-        assertEquals(p1, priorityQueue.maximum());
-    }
+	// Maximum(): test 1
+	@Test
+	public void testCorrectMaximum() {
+		setup2();
+		assert 3 == priorityQueue.size();
+		assertEquals(p1, priorityQueue.maximum());
+	}
 
-    // Maximum(): test2
-    @Test
-    public void testMaximumInEmptyQueue() {
-        setup1();
-        assertNull(priorityQueue.maximum());
-    }
+	// Maximum(): test 2
+	@Test
+	public void testMaximumInEmptyQueue() {
+		setup1();
+		assertNull(priorityQueue.maximum());
+	}
 
-    // Maximum(): test3
-    @Test
-    public void testMaximumAfterExtract() {
-        setup2();
-        assertEquals(p1, priorityQueue.maximum());
-        priorityQueue.extractMax();
-        assertEquals(p2, priorityQueue.maximum());
-        priorityQueue.extractMax();
-        assertEquals(p3, priorityQueue.maximum());
-    }
+	// Maximum(): test 3
+	@Test
+	public void testMaximumAfterExtract() {
+		setup2();
+		assertEquals(p1, priorityQueue.maximum());
+		priorityQueue.extractMax();
+		assertEquals(p2, priorityQueue.maximum());
+		priorityQueue.extractMax();
+		assertEquals(p3, priorityQueue.maximum());
+	}
 
-    // ExtractMax(): test1
-    @Test
-    public void testExtractMaxNonEmptyPriorityQueue() {
-        setup2();
-        assertEquals(p1, priorityQueue.extractMax());
-        assertEquals(p2, priorityQueue.extractMax());
-        assertEquals(p3, priorityQueue.extractMax());
-    }
+	// ExtractMax(): test 1
+	@Test
+	public void testExtractMaxNonEmptyPriorityQueue() {
+		setup2();
+		assertEquals(p1, priorityQueue.extractMax());
+		assertEquals(p2, priorityQueue.extractMax());
+		assertEquals(p3, priorityQueue.extractMax());
+	}
 
-    // ExtractMax(): test2
-    @Test(expected = PriorityQueueException.class)
-    public void testExtractMaxInEmptyPriorityQueue() {
-        setup1();
-        priorityQueue.extractMax();
-    }
+	// ExtractMax(): test 2
+	@Test(expected = PriorityQueueException.class)
+	public void testExtractMaxInEmptyPriorityQueue() {
+		setup1();
+		priorityQueue.extractMax();
+	}
 
-    // ExtractMax(): test3
-    @Test
-    public void testExtractMaxDuplicatePriorityElements() {
-        setup3();
-        assertEquals(p3, priorityQueue.extractMax());
-        assertEquals(p4, priorityQueue.extractMax());
-        assertEquals(p1, priorityQueue.extractMax());
-        assertEquals(p5, priorityQueue.extractMax());
-        assertEquals(p2, priorityQueue.extractMax());
-    }
+	// ExtractMax(): test 3
+	@Test
+	public void testExtractMaxDuplicatePriorityElements() {
+		setup3();
+		assertEquals(p3, priorityQueue.extractMax());
+		assertEquals(p4, priorityQueue.extractMax());
+		assertEquals(p1, priorityQueue.extractMax());
+		assertEquals(p5, priorityQueue.extractMax());
+		assertEquals(p2, priorityQueue.extractMax());
+	}
 
-    // IncreaseKey() test1
-    @Test
-    public void testIncreaseKey() {
-        setup2();
-        priorityQueue.increaseKey(1, 57);
-        assertEquals(p3, priorityQueue.extractMax());
-        assertEquals(p1, priorityQueue.extractMax());
-        assertEquals(p2, priorityQueue.extractMax());
-    }
+	// IncreaseKey() test 1
+	@Test
+	public void testIncreaseKey() {
+		setup2();
+		priorityQueue.increaseKey(1, 57);
+		assertEquals(p3, priorityQueue.extractMax());
+		assertEquals(p1, priorityQueue.extractMax());
+		assertEquals(p2, priorityQueue.extractMax());
+	}
 
-    // IncreaseKey() test2
-    @Test(expected = PriorityQueueException.class)
-    public void testIncreaseKeyInvalidNewKey() {
-        setup1();
-        priorityQueue.insert(42, p1);
-        priorityQueue.increaseKey(0, 23);
-    }
+	// IncreaseKey() test 2
+	@Test(expected = PriorityQueueException.class)
+	public void testIncreaseKeyInvalidNewKey() {
+		setup1();
+		priorityQueue.insert(42, p1);
+		priorityQueue.increaseKey(0, 23);
+	}
 
-    // IncreaseKey() test3
-    @Test(expected = IndexOutOfBoundsException.class)
-    public void testIncreaseKeyInvalidIndex() {
-        setup1();
-        priorityQueue.insert(42, p1);
-        priorityQueue.increaseKey(1, 56);
-    }
+	// IncreaseKey() test 3
+	@Test(expected = IndexOutOfBoundsException.class)
+	public void testIncreaseKeyInvalidIndex() {
+		setup1();
+		priorityQueue.insert(42, p1);
+		priorityQueue.increaseKey(1, 56);
+	}
 
 }
