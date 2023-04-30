668846d22a69413175ac55b56f11299c66de98e5
Santiago <santiesleo@gmail.com>
2023-04-29 19:26:46 -0500

    Separated user interaction menu.

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -17,14 +17,13 @@ public class Main {
         JOptionPane.showMessageDialog(null, "*****************************************************************\n                   WELCOME TO BOARDING SYSTEM                    \n*****************************************************************");
 
         boolean stopFlag = false;
-
+        boolean menuFlag = false;
         while (!stopFlag) {
             String menu = "";
             menu += ("MAIN MENU:");
             menu += ("\n[1] Check in a passenger.");
             menu += ("\n[2] Show passenger entry order.");
-            menu += ("\n[3] Display passenger exit order.");
-            menu += ("\n[4] Exit.");
+            menu += ("\n[3] Exit.");
 
             String input = JOptionPane.showInputDialog(menu);
 
@@ -40,12 +39,13 @@ public class Main {
                         passengerCheckIn();
                         break;
                     case 2:
+                        String alert = "";
+                        alert += ("Once the entry list is displayed, no more passengers can be entered.");
+                        JOptionPane.showMessageDialog(null, alert);
                         showEntryOrder();
+                        stopFlag = subMenu();
                         break;
                     case 3:
-                        showExitOrder();
-                        break;
-                    case 4:
                         String exit = "";
                         exit += ("*****************************************************************\n");
                         exit += ("                              EXIT SUCCESSFULLY                  \n");
@@ -69,6 +69,55 @@ public class Main {
                 JOptionPane.showMessageDialog(null, invalidInput, "Error", JOptionPane.ERROR_MESSAGE);
             }
         }
+    }
+
+    public boolean subMenu() {
+
+        boolean stopFlag = false;
+        while (!stopFlag) {
+            String menu = "";
+            menu += ("MAIN MENU:");
+            menu += ("\n[1] Display passenger exit order.");
+            menu += ("\n[2] Exit.");
+
+            String input = JOptionPane.showInputDialog(menu);
+
+            if (input == null) {
+                stopFlag = true;
+                continue;
+            }
+
+            try {
+                int mainOption = Integer.parseInt(input);
+                switch (mainOption) {
+                    case 1:
+                        showExitOrder();
+                        break;
+                    case 2:
+                        String exit = "";
+                        exit += ("*****************************************************************\n");
+                        exit += ("                              EXIT SUCCESSFULLY                  \n");
+                        exit += ("*****************************************************************");
+                        JOptionPane.showMessageDialog(null, exit);
+                        stopFlag = true;
+                        break;
+                    default:
+                        String invalidInput = "";
+                        invalidInput += ("*****************************************************************\n");
+                        invalidInput += ("            INVALID INPUT: PLEASE ENTER A VALID VALUE            \n");
+                        invalidInput += ("*****************************************************************");
+                        JOptionPane.showMessageDialog(null, invalidInput, "Error", JOptionPane.ERROR_MESSAGE);
+                        break;
+                }
+            } catch (NumberFormatException ex) {
+                String invalidInput = "";
+                invalidInput += ("*****************************************************************\n");
+                invalidInput += ("            INVALID INPUT: PLEASE ENTER A VALID VALUE            \n");
+                invalidInput += ("*****************************************************************");
+                JOptionPane.showMessageDialog(null, invalidInput, "Error", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+        return stopFlag;
     }
 
     public void passengerCheckIn() {
